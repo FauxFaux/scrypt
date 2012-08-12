99cce4ce0b78c8fc498ab236ac3348f7e2543e69
Chris West (Faux) <git@goeswhere.com>
2012-08-12 23:35:31 +0100

    unruin benchmark

--- src/main/java/com/lambdaworks/crypto/Benchmark.java
+++ src/main/java/com/lambdaworks/crypto/Benchmark.java
@@ -1,12 +1,30 @@
 package com.lambdaworks.crypto;
 
+import static com.lambdaworks.crypto.SCrypt.scryptJ;
+import static com.lambdaworks.crypto.SCrypt.scryptN;
+
 import java.security.GeneralSecurityException;
 
 import com.lambdaworks.codec.Base64;
 
 public class Benchmark {
 
-    private static final int BLOCK = 10;
+    private static final byte[] BYTE_16 = new byte[16];
+    private static final int BLOCK = 50;
+
+    private static final Block NATIVE = new Block() {
+        @Override
+        public void run() throws GeneralSecurityException {
+            check(scryptN(BYTE_16, BYTE_16, 16384, 8, 1, 32));
+        }
+    };
+
+    private static final Block JAVA = new Block() {
+        @Override
+        public void run() throws GeneralSecurityException {
+            check(scryptJ(BYTE_16, BYTE_16, 16384, 8, 1, 32));
+        }
+    };
 
     static interface Block {
         void run() throws Exception;
@@ -17,57 +35,33 @@ public class Benchmark {
     static class Timer {
         final long start = System.nanoTime();
         void print() {
-            System.out.println((System.nanoTime() - start) / 1e9 / BLOCK + " seconds per hash");
+            System.out.println((System.nanoTime() - start) / 1e9 / BLOCK + " seconds per kdf");
         }
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("java:");
-        final int runs = 5;
-        for (int i = 0; i < runs; ++i) {
-            time(new Block() {
-                @Override
-                public void run() throws Exception {
-                    runJava();
-                }
-            });
-        }
-
         System.out.println("native:");
-        for (int i = 0; i < runs; ++i) {
-            time(new Block() {
-                @Override
-                public void run() throws Exception {
-                    runNative();
-                }
-            });
-        }
+        time(repeatedly(NATIVE));
+        time(repeatedly(NATIVE));
+
+        System.out.println("java:");
+        time(repeatedly(JAVA));
+        time(repeatedly(JAVA));
     }
 
-    private static void runNative() throws Exception {
-        lots(new Block() {
+    private static Block repeatedly(final Block in) {
+        return new Block() {
             @Override
-            public void run() throws GeneralSecurityException {
-                if (!ANS.equals(new String(Base64.encode(SCrypt.scryptN(new byte[16], new byte[16], 16384, 8, 1, 32)))))
-                    throw new IllegalStateException();
+            public void run() throws Exception {
+                for (int i = 0; i < BLOCK; ++i)
+                    in.run();
             }
-        });
+        };
     }
 
-    private static void runJava() throws Exception {
-        lots(new Block() {
-            @Override
-            public void run() throws GeneralSecurityException {
-                if (!ANS.equals(new String(Base64.encode(SCrypt.scryptJ(new byte[16], new byte[16], 16384, 8, 1, 32)))))
-                    throw new IllegalStateException();
-            }
-        });
-    }
-
-    private static void lots(Block block) throws Exception {
-        for (int i = 0; i < BLOCK; ++i) {
-            block.run();
-        }
+    private static void check(byte[] res) {
+        if (!ANS.equals(new String(Base64.encode(res))))
+            throw new IllegalStateException();
     }
 
     private static void time(Block block) throws Exception {
